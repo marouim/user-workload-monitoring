fcc08f6dca63aa9bc72010971b9f1615c5af94f7
Martin Ouimet <marouim@gmail.com>
2021-08-05 09:48:19 -0400

    Added monitoring metrics

--- monitoring-demo-app/src/main/java/com/rhcasalab/monitoring/demo/app/MonitoringResource.java
+++ monitoring-demo-app/src/main/java/com/rhcasalab/monitoring/demo/app/MonitoringResource.java
@@ -4,13 +4,20 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.inject.Inject;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @Path("/monitoring-demo-app")
 public class MonitoringResource {
 
+    @Inject
+    MeterRegistry registry;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        registry.counter("greeting_counter").increment();
+
         return "Hello RESTEasy";
     }
 }
